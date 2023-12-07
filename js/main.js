2109c89473b4c004305f802444a63f62445f0970
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-12-07 15:13:20 +0500

    zakaz joyi tayyorlandi

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 
+function totalCalc() {
+  const cartItem = document.querySelectorAll('.cart-item')
+  const totalPrice = document.querySelector('.total-price')
+  const deliveryCost = document.querySelector('.delivery-cost')
+  
+  deliveryCost
+
+  let total = 0
+  cartItem.forEach (item => {
+    let count = item.querySelector('[data-counter]').innerText
+    let price = item.querySelector('.price__currency').innerText
+    let totalP =parseInt(count) * parseInt(price)
+    total += totalP
+  })
+  totalPrice.innerHTML = total
+
+  console.log(deliveryCost);
+
+  if (total < "600") {
+    deliveryCost.innerText = "300₽" 
+  } else {
+    deliveryCost.innerText = "бесплатно"
+  }
+}
+
+totalCalc()
+
 const cardWrapper = document.querySelector('.cart-wrapper')
 
 window.addEventListener('click', (event) => {
@@ -6,17 +33,18 @@ window.addEventListener('click', (event) => {
         const counterWrapper = event.target.closest('.counter-wrapper')
         const counter = counterWrapper.querySelector('[data-counter]')
         counter.innerHTML = ++counter.innerHTML
+        totalCalc() 
     }
 
     if (event.target.dataset.action === 'minus') {
         const counterWrapper = event.target.closest('.counter-wrapper')
         const counter = counterWrapper.querySelector('[data-counter]')
-
         if (parseInt(counter.innerHTML) > 1) {
-            counter.innerHTML = --counter.innerHTML
-        } else {
-            counter.innerHTML = '1'
+          counter.innerHTML = --counter.innerHTML
+        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerHTML) == 1) {
+          event.target.closest('.cart-item').remove()
         }
+        totalCalc() 
     }
 
 
@@ -39,51 +67,53 @@ window.addEventListener('click', (event) => {
 
         // console.log(cardItem);
 
-        let itemHTML = `
-        <div class="cart-item" data-id="${cardItem.id}">
-            <div class="cart-item__top">
-                <div class="cart-item__img">
-                    <img src="${cardItem.img}" alt="" />
-                </div>
-                <div class="cart-item__desc">
-                    <div class="cart-item__title">${cardItem.title}</div>
-                    <div class="cart-item__weight">
-                        ${cardItem.itemInBox} / ${cardItem.weight}
-                    </div>
+        const itemCart = cardWrapper.querySelector(`[data-id="${cardItem.id}"]`)
 
-                    <!-- cart-item__details -->
-                    <div class="cart-item__details">
-                    <div class="items items--small counter-wrapper">
-                          <div class="items__control" data-action="minus">
-                            -
-                          </div>
-                          <div class="items__current" data-counter>
-                            ${cardItem.count}
-                          </div>
-                          <div class="items__control" data-action="plus">+</div>
-                        </div>
-
-                        <div class="price">
-                          <div class="price__currency">
-                            ${cardItem.price}
-                          </div>
-                        </div>
+        if (itemCart) {
+          let counterElem = itemCart.querySelector('[data-counter]')
+          counterElem.innerText = parseInt(counterElem.innerText) + parseInt(cardItem.count)
+        } else {
+          let itemHTML = `
+          <div class="cart-item" data-id="${cardItem.id}">
+              <div class="cart-item__top">
+                  <div class="cart-item__img">
+                      <img src="${cardItem.img}" alt="" />
+                  </div>
+                  <div class="cart-item__desc">
+                      <div class="cart-item__title">${cardItem.title}</div>
+                      <div class="cart-item__weight">
+                          ${cardItem.itemInBox} / ${cardItem.weight}
                       </div>
-                      <!-- // cart-item__details -->
+  
+                      <!-- cart-item__details -->
+                      <div class="cart-item__details">
+                      <div class="items items--small counter-wrapper">
+                            <div class="items__control" data-action="minus">
+                              -
+                            </div>
+                            <div class="items__current" data-counter>
+                              ${cardItem.count}
+                            </div>
+                            <div class="items__control" data-action="plus">+</div>
+                          </div>
+  
+                          <div class="price">
+                            <div class="price__currency">
+                              ${cardItem.price}
+                            </div>
+                          </div>
+                        </div>
+                        <!-- // cart-item__details -->
+                      </div>
                     </div>
                   </div>
-                </div>
-        `
+          `
+  
+          cardWrapper.insertAdjacentHTML('beforeend', itemHTML)
+        }
 
-        cardWrapper.insertAdjacentHTML('beforeend', itemHTML)
+        totalCalc() 
         card.querySelector('[data-counter]').innerText = "1"
 
     }
-
-
-
-
-
-
-
 })
